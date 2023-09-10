c38708f31c8f5a0c047ad1ee372a8690e5713c22
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2023-09-10 18:39:09 +0200

    Removed MWSE module from Core

--- 00 Core/mwse/mods/tew/AColdCell/main.lua
+++
@@ -1,9 +0,0 @@
-local function init()
-    -- Custom Icon for Skyrim Style Quest Notifications
-    local ssqn = include("SSQN.interop")
-    if (ssqn)  then
-        ssqn.registerQIcon("tew_acoldcell","\\Icons\\tew\\acoldcell\\quest_acoldcell.tga")
-    end
-end
-
-event.register(tes3.event.initialized, init)
